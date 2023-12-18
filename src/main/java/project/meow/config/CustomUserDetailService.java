43e22c56d801b8b42475a8584e3970a579e46186
MareBelloFiore <gudtjq0306@naver.com>
2023-12-18 22:11:47 +0900

    gradle 파일 추가 및 주석, 패키지 구조 변경

---
+++ src/main/java/project/meow/config/CustomUserDetailService.java
@@ -0,0 +1,4 @@
+package project.meow.config;
+
+public class CustomUserDetailService {
+}
--- src/main/java/project/meow/config/WebConfig.java
+++ src/main/java/project/meow/config/WebConfig.java
@@ -1,4 +1,4 @@
-package project.meow.Config;
+package project.meow.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
